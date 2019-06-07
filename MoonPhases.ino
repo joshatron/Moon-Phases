56ec8a9f20e2b753fa91caee3a1948218107c3ec
Joshua Leger <legermjoshua@gmail.com>
2019-06-07 09:01:53 -0700

    Started building state machine

--- MoonPhases.ino
+++ MoonPhases.ino
@@ -10,6 +10,7 @@ unsigned long time = 0;
 //Pins for lights from left to right
 int statePins[6] = {9,3,10,5,6,11};
 //Pin for button
+//Button is configured as a pull down, so read HIGH for pressed
 int buttonPin = 8;
 
 //Brightness control, from 0 to 1
@@ -24,6 +25,10 @@ int transitionBrightnesses[6][12] = {
  {255,0,0,0,0,0,0,255,255,255,255,255}
 };
 
+enum ButtonState {MODE, OFFSET, BRIGHTNESS};
+ButtonState currentState = MODE;
+int stateIndicator = 0;
+
 void setup() {
   for(int i = 0; i < 6; i++) {
     pinMode(statePins[i], OUTPUT);
@@ -31,7 +36,7 @@ void setup() {
   pinMode(buttonPin, INPUT);
 }
 
-void applyState(double percentThrough) {
+void applyMoonState(double percentThrough) {
   int before = int(percentThrough * 12);
   int after = before + 1;
   double transition = percentThrough * 12 - before;
@@ -44,26 +49,56 @@ void applyState(double percentThrough) {
   }
 }
 
-void loop() {
-  //Using millis() - time makes it work even with the overflow that millis makes
-  if(millis() - time > moonPeriod) {
-    time = millis();
-  }
-
-  //(Time elapsed in period + adding halfway through the state + the offset based on button input) / the period of the state
-  //The halfway through state is so the starting point is right at the full moon to make a good reference point
-  double current = ((millis() - time) + (moonPeriod / 24.) + (offsetAmount * offset)) / moonPeriod;
-  while(current > 1) {
-    current--;
-  }
-
-  applyState(current);
-
-  //Button is configured as a pull down
+void runModeState() {
   if(digitalRead(buttonPin) == HIGH) {
-    //Increase by 1, but loop back down to 0 if more than a full period
-    offset = (offset + 1) % 30;
-    //Do the delay because otherwise it will cycle the offset really quickly
-    delay(500);
+    stateIndicator = (stateIndicator + 1) % 3;
+  }
+  else {
+    switch(stateIndicator) {
+      case 1:
+        currentState = BRIGHTNESS;
+        stateIndicator = 0;
+        return;
+      case 2:
+        currentState = OFFSET;
+        stateIndicator = 0;
+        return;
+    }
+  }
+  
+  if(stateIndicator == 0) {
+    //Using millis() - time makes it work even with the overflow that millis makes
+    if(millis() - time > moonPeriod) {
+      time = millis();
+    }
+
+    //(Time elapsed in period + adding halfway through the state + the offset based on button input) / the period of the state
+    //The halfway through state is so the starting point is right at the full moon to make a good reference point
+    double current = ((millis() - time) + (moonPeriod / 24.) + (offsetAmount * offset)) / moonPeriod;
+    while(current > 1) {
+      current--;
+    }
+
+    applyMoonState(current);
+  }
+  else {
+    for(int i = 0; i < 6; i++) {
+      if(i == stateIndicator - 1) {
+        analogWrite(statePins[i], 255);
+      }
+      else {
+        analogWrite(statePins[i], 0);
+      }
+    }
+  }
+
+  if(digitalRead(buttonPin) == HIGH) {
+    delay(1000);
+  }
+}
+
+void loop() {
+  if(currentState == MODE) {
+    runModeState();
   }
 }
